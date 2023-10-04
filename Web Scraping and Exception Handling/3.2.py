a98ba87034157b516fed6489d2a8c94c6c8349dd
Kishor-Alagappan <kishor.a@emplay.net>
2023-10-04 21:45:40 +0530

    Completed all the Tasks

--- Web Scraping and Exception Handling/3.2.py
+++ Web Scraping and Exception Handling/3.2.py
@@ -1,23 +1,3 @@
-# import requests
-# from bs4 import BeautifulSoup
-
-# url = input('Enter a url of your choice : ')
-# try:
-#     response = requests.get(url)
-#     if response.status_code == 200:
-#         print('')
-#         print(f'Response code from the webpage {url} is {response.status_code}')
-#         print('')
-#         print('Content of the webpage is : ')
-#         print(response.text)
-#     else:
-#         print('')
-#         print(f'Something wrong with the webpage, response code is {response.status_code}')
-# except:
-#     print('')
-#     print('Invalid webpage') 
-
-
 import requests
 
 # URL of the webpage you want to fetch
--- Web Scraping and Exception Handling/3.3.py
+++ Web Scraping and Exception Handling/3.3.py
@@ -1,16 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = input('Enter an URL of your choice : ')
+
+url = "https://en.wikipedia.org/wiki/Main_Page"
 try:
-    response = requests.get(url)
+    response = requests.get(url)    
     if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        links = soup.find_all('a')
+        soup = BeautifulSoup(response.text, 'html.parser')   
+        links = soup.find_all('a')      
         for link in links:
             href = link.get('href')
-            print(href)
+            if href:
+                print("Hyperlink:", href)
     else:
-        print(f'Invalid response code {response.status_code} for : {url}')
-except:
-    print(f'Invalid webpage : {url}')
+        print("Failed to retrieve the webpage. Status code:", response.status_code)
+
+except requests.exceptions.RequestException as e:
+    print("Error:", e)
+except Exception as e:
+    print("An error occurred:", e)
